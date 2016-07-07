95f873ac892b0242e72c6eb2630263e03359b40e
sanyamgarg93 <sanyamgarg93@yahoo.in>
2016-07-07 21:34:37 +0530

    Add project files.

---
+++ WebApplication1/App_Start/BundleConfig.cs
@@ -0,0 +1,46 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WebApplication1
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/commutr").Include(
+                        "~/Scripts/owl.carousel.min.js",
+                        "~/Scripts/jquery.scrollto.min.js",
+                        "~/Scripts/jquery.localScroll.min.js",
+                        "~/Scripts/jquery.parallax-1.1.3.js",
+                        "~/Scripts/skrollr.min.js",
+                        "~/Scripts/jquery.nav.js",
+                        "~/Scripts/jquery.prettyPhoto.js",
+                        "~/Scripts/main.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.min.css",
+                        "~/Content/owl.carousel.css",
+                        "~/Content/prettyPhoto.css",
+                        "~/Content/style.css",
+                        "~/Content/white.css",
+                        "~/Content/responsive.css"));
+        }
+    }
+}
